9a40fc2e06fc5f2b535ea793c4bbb18e16af0e1d
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-10-13 13:10:34 +0200

    Add Landing page with full width

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './index.css';
+
+function LandingPage() {
+  return (
+    <div className='LandingPage'>
+      <div>LASD@SAC'21</div>
+      <div>
+        Track on Lean and Agile Software Development
+        at the 36th ACM Symposium on Applied Computing
+      </div>
+      <div>April 8 - 12, 2021 Cyprus </div>
+    </div>
+  );
+}
+
+export default LandingPage;
