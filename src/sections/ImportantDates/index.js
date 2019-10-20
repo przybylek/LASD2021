cf86977342b64657550a3842760b3395851bcf1b
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-10-20 19:07:36 +0200

    Fix styles for timeline

--- src/sections/ImportantDates/index.js
+++ src/sections/ImportantDates/index.js
@@ -1,48 +1,65 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import './index.css';
 
 function ImportantDates() {
   return (
     <VerticalTimeline>
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: '#333940', color: '#CCE5FE' }}
-        contentArrowStyle={{ borderRight: '7px solid  #CCE5FE' }}
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
         iconStyle={{ background: '#333940', color: '#CCE5FE' }}
-        date="2011 - present"
+        date='November 12, 2019'
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
+        <h3 className="vertical-timeline-element-title">Submission of technical session proposals</h3>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: '#333940', color: '#CCE5FE' }}
-        contentArrowStyle={{ borderRight: '7px solid  #CCE5FE' }}
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
         iconStyle={{ background: '#333940', color: '#CCE5FE' }}
-        date="2011 - present"
+        date="May 15, 2020"
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
+        <h3 className="vertical-timeline-element-title">Paper submission</h3>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: '#333940', color: '#CCE5FE' }}
-        contentArrowStyle={{ borderRight: '7px solid  #CCE5FE' }}
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
         iconStyle={{ background: '#333940', color: '#CCE5FE' }}
-        date="2011 - present"
+        date="June 9, 2020"
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
+        <h3 className="vertical-timeline-element-title">Position paper submission</h3>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
+        iconStyle={{ background: '#333940', color: '#CCE5FE' }}
+        date="June 30, 2020"
+      >
+        <h3 className="vertical-timeline-element-title">Author notification</h3>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
+        iconStyle={{ background: '#333940', color: '#CCE5FE' }}
+        date="July 15, 2020"
+      >
+        <h3 className="vertical-timeline-element-title">
+          Final paper submission and registration
+        </h3>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        animate
+        contentStyle={{ background: '#333940' , color: '#CCE5FE' }}
+        contentArrowStyle={{ borderRight: '7px solid  #333940'}}
+        iconStyle={{ background: '#333940', color: '#CCE5FE' }}
+        date="September 6-9, 2020"
+      >
+        <h3 className="vertical-timeline-element-title">Conference date</h3>
       </VerticalTimelineElement>
     </VerticalTimeline>
   );
