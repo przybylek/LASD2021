5d26f926d86282c0b845f6fdc30ffd3b8c5a7ac3
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-10-25 21:21:55 +0200

    Add X

---
+++ src/components/X/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css';
+
+function X() {
+  return (
+    <div className='X'>
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
+
+export default X;
