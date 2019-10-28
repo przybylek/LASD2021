232e5a3e0187f991bf1e77bb9ca7c439631b03d4
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-10-29 00:11:53 +0100

    Add Footer

---
+++ src/sections/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+function Footer() {
+  return (
+    <div className='Footer'>
+      Design: Kamil Mastalerz
+    </div>
+  );
+}
+
+export default Footer
