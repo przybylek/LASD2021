6e1cf4df72188ae9b4496870dd7f74351941b9a9
Adam Przybylek <adam.przybylek@gmail.com>
2020-08-24 11:49:57 +0200

    topics updated

--- src/sections/TopicsInterests/index.js
+++ src/sections/TopicsInterests/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { List, Icon } from 'semantic-ui-react'
 
 const topics = [
+  'Agile teams during Covid-19',
   'Combining lean and agile methods for software development',
   'Lean and agile requirements engineering',
   'Scaling agile methods',
-  'Distributed agile software development',
+  'Distributed teams in Agile Software Development',
   'Challenges of migrating to lean and agile methods',
   'Balancing agility and discipline',
   'Agile development for safety systems',
   'Lean and agility at the enterprise level',
   'Conflicts in agile teams',
   'Lean and agile project production and management',
-  'Collaborative games in software processes',
+  'Collaborative games in Software Process Improvement',
   'Lean and agile coaching',
   'Managing knowledge for agility and collaboration',
   'Tools and techniques for lean and agile development',
