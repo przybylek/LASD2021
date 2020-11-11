ef8934ec61ea28be95e9d916c44417349efe7eaa
Adam Przybylek <adam.przybylek@gmail.com>
2020-11-11 15:47:58 +0100

    Keynote titles added

--- src/sections/Keynotes/index.js
+++ src/sections/Keynotes/index.js
@@ -5,17 +5,26 @@ import './index.css';
 function Keynotes({history}) {
   return (
     <div className='PaperSubmission'>
-	<p>
-	<dl>
-	  <dt>Philipp Diebold</dt>
-	  <dd>Before starting his own consulting, coaching and training company, called <a href="https://www.bagilstein.de/">Bagilstein</a>, Philipp was working as a project manager and researcher at the Fraunhofer Institut for Experimental Software Engineering IESE. Working in the area of agile processes, methods and practices, he published a lot around this topics in different conferences, journals and books. He finished his PhD on identifying the right degree of agility at the Technical University of Kaiserslautern. With his own company his is now coaching different customers for more than 2 years in agile transformations still keeping the eye on the leading edge of research.</dd>
-  </dl>
-  </p>
+	
+<p>
 
-  <p>
-	<dl>
-    <dt>Sanjay Misra</dt>
-	  <dd>
+<h1>Philipp Diebold</h1>
+<dl>
+<dt>Keynote title</dt>
+<dd>Agility Yesterday, Today & Tomorrow</dd>
+
+<dt>Short Biography</dt>
+<dd>Before starting his own consulting, coaching and training company, called <a href="https://www.bagilstein.de/">Bagilstein</a>, Philipp was working as a project manager and researcher at the Fraunhofer Institut for Experimental Software Engineering IESE. Working in the area of agile processes, methods and practices, he published a lot around this topics in different conferences, journals and books. He finished his PhD on identifying the right degree of agility at the Technical University of Kaiserslautern. With his own company his is now coaching different customers for more than 2 years in agile transformations still keeping the eye on the leading edge of research.
+</dd>
+</dl>
+
+<h1>Sanjay Misra</h1>
+<dl>
+<dt>Keynote title</dt>
+<dd>Pair Programming: An Empirical investigation in an Agile Software Development environment</dd>
+
+<dt>Short Biography</dt>
+<dd>
     <a href="https://covenantuniversity.edu.ng/Profiles/Misra-Sanjay">Sanjay Misra</a> is full Professor of Computer(software) Engineering at Covenant University (
 <a href="https://www.timeshighereducation.com/world-university-rankings/covenant-university">400-500 ranked University</a>) Ota, Nigeria. He has 25 years of wide experience in academic administration and research in various universities in Asia, Europe, and Africa. He is PhD. in Information and Know. Engg (Software Engineering) from the University of Alcala, Spain and M.Tech. (Software Engineering) from Motilal Nehru National Institute of Technology, India. As of today (15.03.2020)- as per SciVal (SCOPUS- Elsevier analysis)- he is the most productive researcher <a href="https://www.scival.com/overview/authors?uri=Country/566">(no. 1-)</a> in whole Nigeria during 2012-2017, 2013-2018 and 2014-2019 (<a href="https://t.co/fBYnVxbmiL">in all subjects</a>), in computer science <a href="https://www.scival.com/overview/authors?uri=Country/566">no 1 in the whole country</a> and <a href="https://www.scival.com/overview/authors?uri=CountryGroup/1">no 4 in the whole continent</a>.
 Total around <a href="https://scholar.google.com/citations?user=AFjvjTgAAAAJ&amp;hl=en">400 articles</a> (SCOPUS/Web of Science) with 200 coauthors around the world (-90 in JCR/SCIE Journals) in the core & application area of Software Engg (SQA, SPI, SPM), Web engg, Health Informatics, Intelligent systems etc.
@@ -32,10 +41,9 @@ He is a software engineer and previously held academic positions at FUT Minna - 
 <br /><br />
 
 His current research covers the areas of software quality assurance, software process improvement, software project management, object-oriented technologies, XML, SOA, Web services, cognitive informatics, artificial intelligence, neural network, health Informatics, e-learning, cloud computing, and cybersecurity. Prof Sanjay is coordinating/working/collaborating with several research groups (as visiting/adjunct professor/collaborative researcher) in various universities and industry around the world (Spain, Argentina, Turkey, Singapore, Chile, Brazil, Mexico, Norway, France, South Africa, Malaysia, Nigeria, Myanmar, Vietnam, India).
-    </dd>
-	</dl>
-	</p>
-
+</dd>
+</dl>
+</p>
 
 
     </div>
