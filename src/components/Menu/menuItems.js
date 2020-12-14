7c358f84c3ebd04f02fe12ac0236371fb84b20c2
Adam Przybylek <adam.przybylek@gmail.com>
2020-12-14 14:44:49 +0100

    CSS for the Program section added

--- src/components/Menu/menuItems.js
+++ src/components/Menu/menuItems.js
@@ -4,6 +4,7 @@ export const menuItems = [
   { scrollTo: 'submissionsSection', text: 'Submissions' },
   { scrollTo: 'datesSection', text: 'Dates' },
   { scrollTo: 'keynotesSection', text: 'Keynotes' },
+  { scrollTo: 'programSection', text: 'Program' },
   { scrollTo: 'chairsSection', text: 'PC' },
   { scrollTo: 'pastConferenciesSection', text: 'Past Conferences' }
 ]
