78441b0d96417a4cbd3930a566a40523f832bdc5
Adam Przybylek <adam.przybylek@gmail.com>
2020-12-29 11:24:35 +0100

    current live datetime added

--- src/sections/Program/index.js
+++ src/sections/Program/index.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import Moment from 'react-moment';
+
 import './index.css';
 
-function Program({history}) {
-  return (
+
+class Program extends React.Component {
+  state = new Date();
+
+  componentDidMount() {
+        this.interval = setInterval(() => {
+            const now = new Date();
+            this.setState(now);
+        }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+        const localDataTime = this.state;
+      
+        return (
     <div className='PaperSubmission'>
+
+    <p>Time zone of the conference: GMT+1 (Central European Standard Time)</p>
+    <p><Moment interval={1000} format="YYYY/MM/DD hh:mm:ss">{localDataTime}</Moment></p>
+
 
     <table border="1">
       <tr><th colspan="2">Session 1 (Chair: <span class='author'>...</span>)</th></tr>
-      <tr><td>11:10 - 11:20</td><td>Conference opening; <span class='author'>Adam Przybyłek</span></td></tr>
-	  <tr><td>11:20 - 12:15</td><td>Agility Yesterday, Today & Tomorrow; <span class='author'>Philipp Diebold</span></td></tr>
+      <tr><td>11:10 - 11:20</td><td>Conference opening<br /><span class='author'>Adam Przybyłek</span></td></tr>
+	  <tr><td>11:20 - 12:15</td><td>Agility Yesterday, Today & Tomorrow (followed by a 10-minute discussion)<br /><span class='author'>Philipp Diebold</span><br /></td></tr>
 	  <tr><td>12:15 - 13:00</td><td>Q&A:
 		<ul>
 			<li>Women in Agile: The Impact of Organizational Support for Women's Advancement on Teamwork Quality and Performance in Agile Software Development Teams<br /><span class='author'>Asli Yüksel Aksekili and Christoph Johann Stettina</span></li>
 			<li>The state of Agile Software Development Teams during the Covid-19 pandemic<br /><span class='author'>Krzysztof Marek, Ewelina Wińska and Włodzimierz Dąbrowski</span></li>
-			<li>The Sars-Cov-2 Pandemic and Agile Methodologies in Software Development: A Multiple Case Study in Germany<br /><span class='author'>Michael Neumann, Yevgen Bogdanov, Martin Lier and Lars Baumann</span></li>
+			<li><a href="https://youtu.be/_IJ_RmxBnbo">The Sars-Cov-2 Pandemic and Agile Methodologies in Software Development: A Multiple Case Study in Germany</a><br /><span class='author'>Michael Neumann, Yevgen Bogdanov, Martin Lier and Lars Baumann</span></li>
 			<li>Agile Project Development Facing Issues in Pandemic Situation the COVID-19<br /><span class='author'>Shariq Aziz Butt, Sanjay Misra, Muhammad Waqas Ajnum and Syed Areeb Hassan</span></li>
 			<li>Implementing Lean Principles in Scrum to Adapt to Remote Work in a Covid-19 Impacted Software Team<br /><span class='author'>Leigh Griffin</span></li>
 			<li>MSFL: A model for Fault Localization using Mutation-Spectra technique<br /><span class='author'>Arpita Dutta and Sangharatna Godboley</span></li>
@@ -21,7 +46,7 @@ function Program({history}) {
 	  </td></tr>
       <tr><th colspan="2">Coffee break </th></tr>
       <tr><th colspan="2">Session 2 (Chair: <span class='author'>...</span>)</th></tr>      
-	  <tr><td>14:00 - 14:55</td><td>Pair Programming: An Empirical investigation in an Agile Software Development environment <br /><span class='author'>Sanjay Misra</span></td></tr>
+	  <tr><td>14:00 - 14:55</td><td>Pair Programming: An Empirical investigation in an Agile Software Development environment (followed by a 10-minute discussion)<br /><span class='author'>Sanjay Misra</span></td></tr>
 	  <tr><td>14:55	- 15:40</td><td>Q&A:
 		<ul>
 			<li>Achieving Agility in IT Project Portfolios – a systematic literature review<br /><span class='author'>Joseph Puthenpurackal Chakko, Tim Huygh and Steven De Haes</span></li>
@@ -33,11 +58,11 @@ function Program({history}) {
 		</ul>
 	  </td></tr>
 	  <tr><td>15:40 - 15:50</td><td>Conference closing </td></tr>
-    </table>	
- 
-  
+    </table>	 
     </div>
-  );
+        );
+  }
+
 }
 
 export default Program;
