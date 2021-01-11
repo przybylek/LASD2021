af6945efdfd02eee6568932ab55664c4e6c20c79
Adam Przybylek <adam.przybylek@gmail.com>
2021-01-11 14:06:36 +0100

    Session chairs added

--- src/sections/Program/index.js
+++ src/sections/Program/index.js
@@ -31,7 +31,7 @@ class Program extends React.Component {
 
 
     <table border="1">
-      <tr><th colspan="2">Session 1 (Chair: <span class='author'>...</span>)</th></tr>
+      <tr><th colspan="2">Session 1 (Chair: <span class='author'>Aleksander Jarzębowicz</span>)</th></tr>
       <tr><td>11:10 - 11:20</td><td>Conference opening<br /><span class='author'>Adam Przybyłek</span></td></tr>
 	  <tr><td>11:20 - 12:15</td><td>Agility Yesterday, Today & Tomorrow (followed by a 10-minute discussion)<br /><span class='author'>Philipp Diebold</span><br /></td></tr>
 	  <tr><td>12:15 - 13:00</td><td>Q&A:
@@ -45,7 +45,7 @@ class Program extends React.Component {
 		</ul>
 	  </td></tr>
       <tr><th colspan="2">Coffee break </th></tr>
-      <tr><th colspan="2">Session 2 (Chair: <span class='author'>...</span>)</th></tr>      
+      <tr><th colspan="2">Session 2 (Chair: <span class='author'>Jakub Miler</span>)</th></tr>      
 	  <tr><td>14:00 - 14:55</td><td>Pair Programming: An Empirical investigation in an Agile Software Development environment (followed by a 10-minute discussion)<br /><span class='author'>Sanjay Misra</span></td></tr>
 	  <tr><td>14:55	- 15:40</td><td>Q&A:
 		<ul>
@@ -53,7 +53,7 @@ class Program extends React.Component {
 			<li><a href="https://youtu.be/PjQ8U_DxUaA">A Systematic Literature Review on Implementing Non-functional Requirements in Agile Software Development: Issues and Facilitating Practices</a><br /><span class='author'>Aleksander Jarzębowicz and Paweł Weichbroth</span></li>
 			<li><a href="https://youtu.be/z5eRZzDp_sg">Product Roadmapping Processes for an Uncertain Market Environment: A Grey Literature Review</a><br /><span class='author'>Stefan Trieflinger, Jürgen Münch, Emre Bogazköy, Jan Schneider, Patrick Eißler, Bastian Roling and Dominic Lang</span></li>
 			<li><a href="https://youtu.be/F_7E2KoRh_4">Experience vs Data: A Case for More Data-informed Retrospective Activities</a><br /><span class='author'>Christoph Matthies and Franziska Dobrigkeit</span></li>
-			<li>Reducing the uncertainty of agile software development using a random forest classification algorithm<br /><span class='author'>Ewelina Wińska, Estera Kot and Włodzimierz Dąbrowski</span></li>
+			<li><a href="https://youtu.be/MGivIfk-_pE">Reducing the uncertainty of agile software development using a random forest classification algorithm</a><br /><span class='author'>Ewelina Wińska, Estera Kot and Włodzimierz Dąbrowski</span></li>
 			<li>Business-Oriented approach to Requirements Elicitation in a Scrum project<br /><span class='author'>Michał Sosnowski, Michał Bereza, Yen Ying Ng</span></li>
 		</ul>
 	  </td></tr>
